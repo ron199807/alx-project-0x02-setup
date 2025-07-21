8122dd26938a5e88588b95c57685946c15f3647f
ron19907 <ronaldmweema@gmail.com>
2025-07-21 15:13:45 +0200

    imported link

--- alx-project-0x02/components/layout/Header.tsx
+++ alx-project-0x02/components/layout/Header.tsx
@@ -1,11 +1,19 @@
+import Link from "next/link"
+
 export const Header = () => {
     return (
-         <div>
-                <ul className="flex gap-4 ">
-                    <li><a href="/home">Home</a></li>
-                    <li><a href="/about">About</a></li>
-                    <li><a href=""></a></li>
-                </ul>
+        <div className="flex items-center space-x-6">
+          <nav className="flex items-center space-x-6">
+            <Link href="/home" className="hover:text-blue-500">
+              Home
+            </Link>
+            <Link href="/about" className="hover:text-blue-500">
+              About
+            </Link>
+            <Link href="/contact" className="hover:text-blue-500">
+              Contact
+            </Link>
+          </nav>
          </div>
     )
 }
