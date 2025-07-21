95d2d9deeff8a802ed1a32fc05b0ce97cf1c1bc4
ron19907 <ronaldmweema@gmail.com>
2025-07-21 14:02:23 +0200

    installed the setup of the next js project with typescript and tailwind

---
+++ alx-project-2/components/layout/Header.tsx
@@ -0,0 +1,9 @@
+export const Header = () => {
+    return (
+        <div>
+            <button>Home</button>
+            <button>Contacts</button>
+            <button>About</button>
+        </div>
+    )
+}
---
+++ alx-project-2/pages/index.tsx
@@ -0,0 +1,11 @@
+import { NextPage } from "next";
+
+const Home: NextPage = () => {
+  return (
+    <div className="flex justify-center item-center h-full w-full bg-blue-500">
+      <h1>Welcome to the Home Page</h1>
+    </div>
+  );
+};
+
+export default Home;
