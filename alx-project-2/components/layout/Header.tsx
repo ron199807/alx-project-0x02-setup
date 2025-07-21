77715f07100a5028b83ff2da0f7e04fed0b8906e
Ronald Mweema <69164861+ron199807@users.noreply.github.com>
2025-07-21 14:10:09 +0200

    Delete alx-project-2 directory

--- alx-project-2/components/layout/Header.tsx
+++
@@ -1,9 +0,0 @@
-export const Header = () => {
-    return (
-        <div>
-            <button>Home</button>
-            <button>Contacts</button>
-            <button>About</button>
-        </div>
-    )
-}
